8cda4653ee7874c8e8b4738d631c386ecc2f1a8c
arvalon <me@arvalon.ru>
2019-01-23 20:49:30 +0300

    Init commit

---
+++ app/src/main/java/info/androidhive/materialtabs/activity/MainActivity.java
@@ -0,0 +1,60 @@
+package info.androidhive.materialtabs.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import info.androidhive.materialtabs.R;
+
+/** https://www.androidhive.info/2015/09/android-material-design-working-with-tabs/ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Toolbar toolbar;
+    private Button btnSimpleTabs, btnScrollableTabs, btnIconTextTabs, btnIconTabs, btnCustomIconTextTabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnSimpleTabs = findViewById(R.id.btnSimpleTabs);
+        btnScrollableTabs = findViewById(R.id.btnScrollableTabs);
+        btnIconTextTabs = findViewById(R.id.btnIconTextTabs);
+        btnIconTabs = findViewById(R.id.btnIconTabs);
+        btnCustomIconTextTabs = findViewById(R.id.btnCustomIconTabs);
+
+        btnSimpleTabs.setOnClickListener(this);
+        btnScrollableTabs.setOnClickListener(this);
+        btnIconTextTabs.setOnClickListener(this);
+        btnIconTabs.setOnClickListener(this);
+        btnCustomIconTextTabs.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSimpleTabs:
+                startActivity(new Intent(MainActivity.this, SimpleTabsActivity.class));
+                break;
+            case R.id.btnScrollableTabs:
+                startActivity(new Intent(MainActivity.this, ScrollableTabsActivity.class));
+                break;
+            case R.id.btnIconTextTabs:
+                startActivity(new Intent(MainActivity.this, IconTextTabsActivity.class));
+                break;
+            case R.id.btnIconTabs:
+                startActivity(new Intent(MainActivity.this, IconTabsActivity.class));
+                break;
+            case R.id.btnCustomIconTabs:
+                startActivity(new Intent(MainActivity.this, CustomViewIconTextTabsActivity.class));
+                break;
+        }
+    }
+}
